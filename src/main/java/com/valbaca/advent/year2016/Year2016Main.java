808c01624bf9841a492e3aa88779e3680e19bbde
Valentin Baca <valbaca@gmail.com>
2021-09-30 22:54:55 -0700

    Year 2016: Day 11 done

--- src/main/java/com/valbaca/advent/year2016/Year2016Main.java
+++ src/main/java/com/valbaca/advent/year2016/Year2016Main.java
@@ -3,5 +3,6 @@ package com.valbaca.advent.year2016;
 public class Year2016Main {
     public static void main(String[] args) {
         System.out.println("Year 2016");
+        Day11.main(args);
     }
 }
