604f72aa6e6e4fabda92a37ee180dbffd6d70054
Valentin Baca <valbaca@gmail.com>
2021-10-03 17:24:40 -0700

    Day 13 done

---
+++ src/main/java/com/valbaca/advent/year2016/Day13.java
@@ -0,0 +1,155 @@
+package com.valbaca.advent.year2016;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import lombok.SneakyThrows;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+import static com.valbaca.advent.year2016.Day13.Spot.Open;
+
+public class Day13 {
+    private PriorityQueue<Path> pq;
+    private HashSet<Coord> seen;
+    private SpotCache cache;
+
+    public static void main(String[] args) {
+        System.out.println("Day 13");
+//        testing();
+        /*
+
+        Part 1 Pseudocode:
+        1. wrap the isWall(x,y) in a cache?
+        2. A* toward the goal?
+            q = Queue<Items>.of(start) // Items is sortable by steps, then by "best"
+            while (q) {
+              curr = q.pop
+              if (curr.solution) handleSolution(curr)
+              visited = curr
+              q.add(curr.options)
+            }
+         */
+
+        System.out.println("Test");
+        {
+            Coord target = new Coord(7, 4);
+            int input = 10;
+            new Day13().run(target, input);
+        }
+        System.out.println("Part 1");
+        {
+            Coord target = new Coord(31, 39);
+            int input = 1350;
+            new Day13().run(target, input);
+        }
+
+    }
+
+
+    private void run(final Coord target, final int input) {
+        pq = new PriorityQueue<Path>(Comparator.comparingInt(Path::steps).thenComparingInt(o -> o.distTo(target)));
+        seen = new HashSet<Coord>();
+        cache = new SpotCache(input);
+
+        var init = new Path(1, 1, 0);
+        pq.add(init);
+        seen.add(init.toCoord());
+
+        while (!pq.isEmpty()) {
+            var curr = pq.poll();
+            if (curr.distTo(target) == 0) {
+                System.out.println("Steps=" + curr.steps);
+                break;
+            }
+            var steps = curr.steps() + 1;
+            visit(new Path(curr.x(), curr.y() - 1, steps)); // up
+            visit(new Path(curr.x() + 1, curr.y(), steps)); // right
+            visit(new Path(curr.x(), curr.y() + 1, steps)); // down
+            visit(new Path(curr.x() - 1, curr.y(), steps)); // left
+        }
+
+    }
+
+    private void visit(Path path) {
+        if (path.x() < 0 || path.y() < 0) return;
+        var coord = path.toCoord();
+
+        if (seen.contains(coord)) return;
+        seen.add(coord);
+
+        if (cache.getSpot(coord) == Spot.Wall) return;
+        pq.add(path);
+    }
+
+    private static void testing() {
+        System.out.println("Testing");
+        var spotCache = new SpotCache(10);
+        for (int y = 0; y < 100; y++) {
+            for (int x = 0; x < 100; x++) {
+                var spotType = spotCache.getSpot(x, y);
+                System.out.print(spotType == Open ? '.' : '#');
+            }
+            System.out.println();
+        }
+    }
+
+    record Coord(int x, int y) {
+    }
+
+    record Path(int x, int y, int steps) {
+        public Path fromCoord(Coord c, int steps) {
+            return new Path(c.x(), c.y(), steps);
+        }
+
+        public static Coord fromPath(Path p) {
+            return new Coord(p.x(), p.y());
+        }
+
+        public Coord toCoord() {
+            return fromPath(this);
+        }
+
+        public int distTo(Coord c) {
+            return Math.abs(x - c.x()) + Math.abs(y - c.y());
+        }
+    }
+
+    enum Spot {
+        Open, Wall;
+
+        public static Spot getSpotType(int x, int y, int input) {
+            long calc = x * x + 3 * x + 2 * x * y + y + y * y + input;
+            var bitCount = Long.bitCount(calc);
+            return ((bitCount & 1) != 1) ? Open : Wall;
+        }
+    }
+
+    static class SpotCache {
+        private final int input;
+        private final CacheLoader<Coord, Spot> loader;
+        private final LoadingCache<Coord, Spot> cache;
+
+        public SpotCache(int input) {
+            this.input = input;
+            this.loader = new CacheLoader<Coord, Spot>() {
+                @Override
+                public Spot load(Coord key) throws Exception {
+                    return Spot.getSpotType(key.x(), key.y(), input);
+                }
+            };
+            cache = CacheBuilder.newBuilder().maximumSize(1000).build(loader);
+        }
+
+        @SneakyThrows
+        public Spot getSpot(Coord c) {
+            return cache.get(c);
+        }
+
+        public Spot getSpot(int x, int y) {
+            return this.getSpot(new Coord(x, y));
+        }
+    }
+}
