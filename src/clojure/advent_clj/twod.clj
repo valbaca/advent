f11057e9c155c40075c98f6ca6fe3eabc00c23e5
Valentin Baca <valbaca@gmail.com>
2021-10-29 16:00:37 -0700

    Day 2 done; added twod for grid-related problems

---
+++ src/clojure/advent_clj/twod.clj
@@ -0,0 +1,48 @@
+(ns advent-clj.twod
+  (:require [clojure.string :as s]))
+
+;; help when working with vectors of vectors
+;; calling them grids
+;; row col is the best way to deal with them
+;; row = y = first index
+;; col = x = second index
+
+;; Return nil instead of throwing out of bounds
+
+(defn ggrid "get-grid"
+  ([g rc] (when rc (apply ggrid g rc)))
+  ([g r c]
+   (let [row (nth g r nil)
+         elem (nth row c nil)]
+     elem)))
+
+(def dirs
+  {:up    (fn [r c] [(dec r) c])
+   :down  (fn [r c] [(inc r) c])
+   :left  (fn [r c] [r (dec c)])
+   :right (fn [r c] [r (inc c)])})
+
+(defn move [dir grid r c]
+  (let [new-rc ((dirs dir) r c)]
+    (when (ggrid grid new-rc)
+      new-rc)))
+
+(def up (partial move :up))
+(defn get-up [grid r c] (ggrid grid (up grid r c)))
+(def down (partial move :down))
+(defn get-down [grid r c] (ggrid grid (down grid r c)))
+(def left (partial move :left))
+(defn get-left [grid r c] (ggrid grid (left grid r c)))
+(def right (partial move :right))
+(defn get-right [grid r c] (ggrid grid (right grid r c)))
+
+(defn get-xy [grid x y] (ggrid grid y x))
+
+(def test-grid
+  (for [x (range 3)]
+    (range (* 10 x) (+ 3 (* 10 x)))))
+
+
+(defn strs->grid [strs] (mapv vec strs))
+
+(defn s->grid [s] (-> s s/split-lines strs->grid))
--- src/clojure/advent_clj/year2016/day2.clj
+++ src/clojure/advent_clj/year2016/day2.clj
@@ -1,16 +1,12 @@
 (ns advent-clj.year2016.day2
-  (:require [advent-clj.elf :refer :all]))
-
+  (:require [advent-clj.elf :refer :all]
+            [advent-clj.twod :refer :all]))
 
 (def input (map seq (ns-input)))
-
 
 ;; UDLR
 (def dir->idx {\U 0 \D 1 \L 2 \R 3})
 
-"1 2 3
- 4 5 6
- 7 8 9"
 (def next-num9
   {\1 [\1 \4 \1 \2]
    \2 [\2 \5 \1 \3]
@@ -33,12 +29,6 @@
 
 (defn part1 [] (solve next-num9))
 
-"    1
-   2 3 4
- 5 6 7 8 9
-   A B C
-     D"
-
 (def next-numD
   {\1 [\1 \3 \1 \1]
    \2 [\2 \6 \2 \3]
@@ -55,3 +45,33 @@
    \D [\B \D \D \D]})
 
 (defn part2 [] (solve next-numD))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Solving again with twod.clj
+(def dir->fn {\U up \D down \L left \R right})
+
+(defn grid-move [[grid r c] d]
+  (let [new-rc ((dir->fn d) grid r c)]
+    (if (and new-rc (not= \space (ggrid grid new-rc)))
+      [grid (first new-rc) (second new-rc)]
+      [grid r c])))
+
+(defn grid-travel
+  [grid start-r start-c]
+  (fn [input]
+    (let [[_ r c] (reduce grid-move [grid start-r start-c] input)]
+      (ggrid grid r c))))
+
+(def num9 "123
+456
+789")
+(defn part1grid [] (map (grid-travel (s->grid num9) 1 1) input))
+
+(def numD
+  "  1
+   234
+  56789
+   ABC
+    D")
+(defn part2grid [] (map (grid-travel (s->grid numD) 2 0) input))
